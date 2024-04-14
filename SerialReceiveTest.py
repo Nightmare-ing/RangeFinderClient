2bfc226464fba7eb61216ab79474b40c93e5083a
smile <kingsanhu@gmail.com>
2024-04-14 22:52:26 +0800

    Finish code for reading TDC data from serial

--- SerialReceiveTest.py
+++ SerialReceiveTest.py
@@ -1,8 +1,17 @@
 import serial
 
+TDC_INNER_REF_CLK = 5  # in MHz
+
+
+def cal_dist(time):
+    dist_per_clk = 300.0 / TDC_INNER_REF_CLK
+    return dist_per_clk * time / 2.0
+
+
 ser = serial.Serial('COM6', 115200, timeout=1)
 
 while True:
     TOF_raw_data = ser.read(4)
     TOF_value = int.from_bytes(TOF_raw_data[0:2]) + int.from_bytes(TOF_raw_data[2:]) / 65536.0
-    print(TOF_value)
+    dist = cal_dist(TOF_value)
+    print(dist)
