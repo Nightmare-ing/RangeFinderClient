91eda97a129cb45220dd02193b812aad62bcae90
smile <kingsanhu@gmail.com>
2024-04-15 15:36:36 +0800

    Finish version using FuncAnimation

--- main.py
+++ main.py
@@ -3,10 +3,25 @@ import matplotlib.animation as animation
 import numpy as np
 import serial
 
+
+def update(frame):
+    angle_data.append(np.random.normal(np.pi / 2, 1))
+    dist_data.append(get_dist())
+    scat.set_offsets(np.stack([angle_data, dist_data], axis=1))
+    return [scat,]
+
+
+def get_dist():
+    tof_raw_data = ser.read(4)
+    tof_value = int.from_bytes(tof_raw_data[0:2]) + int.from_bytes(tof_raw_data[2:]) / 65536.0
+    dist_per_clk = 300.0 / TDC_INNER_REF_CLK
+    dist = dist_per_clk * tof_value / 2.0
+    return dist
+
+
 TDC_INNER_REF_CLK = 5  # in MHz
 
 ser = serial.Serial('COM6', 115200, timeout=1)
-
 fig, ax = plt.subplots(layout='constrained', subplot_kw=dict(projection='polar'))
 
 angle_data = []
@@ -18,46 +33,5 @@ ax.set_yticks(np.arange(0, 4.0, 0.5))
 ax.set_title("Scan result of RangeFinder")
 fig.legend(loc='outside lower center')
 
-
-def update(frame):
-    print(frame)
-    angle_data[frame] = np.random.normal(np.pi / 2, 1)
-    dist_data[frame] = get_dist()
-    data = np.stack([angle_data[:frame], dist_data[:frame]], axis=1)
-    scat.set_offsets(data)
-    return scat
-
-
-def get_dist():
-    tof_raw_data = ser.read(4)
-    tof_value = int.from_bytes(tof_raw_data[0:2]) + int.from_bytes(tof_raw_data[2:]) / 65536.0
-    dist_per_clk = 300.0 / TDC_INNER_REF_CLK
-    dist = dist_per_clk * tof_value / 2.0
-    print(dist)
-    return dist
-
-
-# ani = animation.FuncAnimation(fig, update, frames=1000, interval=0.1, repeat=False)
-# plt.show()
-
-# (points, ) = ax.plot([0], [0], animated=True)
-plt.show(block=False)
-plt.pause(0.1)
-background = fig.canvas.copy_from_bbox(fig.bbox)
-ax.draw_artist(scat)
-fig.canvas.blit(fig.bbox)
-
-for i in range(10000):
-    # print(dist, i)
-    angle_data.append(np.random.normal(np.pi / 2, 1))
-    dist_data.append(get_dist())
-    fig.canvas.restore_region(background)
-    scat.set_offsets(np.stack([angle_data, dist_data], axis=1))
-    ax.draw_artist(scat)
-    fig.canvas.blit(fig.bbox)
-    fig.canvas.flush_events()
-
-    # scat.set_offsets(np.c_[angle_data, dist_data])
-    # plt.pause(0.0000001)
-
+ani = animation.FuncAnimation(fig, update, interval=0.1, frames=10000, repeat=False, blit=True)
 plt.show()
