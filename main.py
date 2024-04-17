8c4919da05831000a6411e4fa4f0538bc700fb42
smile <kingsanhu@gmail.com>
2024-04-17 16:26:37 +0800

    Use multi-threading to read data sent from serial port

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ import matplotlib.animation as animation
 import numpy as np
 from collections import deque
 import serial
+import threading
 
 
 def update(frame):
-    angle_data.append(np.random.normal(np.pi / 2, 1))
-    dist_data.append(get_dist())
     scat.set_offsets(np.stack([angle_data, dist_data], axis=1))
     return [scat,]
 
@@ -20,20 +19,33 @@ def get_dist():
     return dist
 
 
+def read_data():
+    while True:
+        angle_data.append(np.random.normal(np.pi / 2.0, 1))
+        dist_data.append(get_dist())
+
+
 TDC_INNER_REF_CLK = 5  # in MHz
 
-ser = serial.Serial('COM5', 115200, timeout=1)
+ser = serial.Serial('COM9', 115200, timeout=1)
 fig, ax = plt.subplots(layout='constrained', subplot_kw=dict(projection='polar'))
 
-angle_data = deque(maxlen=100)
-dist_data = deque(maxlen=100)
+angle_data = deque([0.0] * 100, maxlen=100)
+dist_data = deque([0.0] * 100, maxlen=100)
 scat = ax.scatter(angle_data, dist_data, c='r', marker='^', label='scanned points', animated=True)
 
+
 ax.set_xticks(np.linspace(0, 2 * np.pi, 12, endpoint=False))
-ax.set_yticks(np.arange(0, 4.0, 0.5))
+ax.set_yticks(np.arange(0.3, 1.3, 0.1))
 ax.set_title("Scan result of RangeFinder")
 fig.legend(loc='outside lower center')
 
-ani = animation.FuncAnimation(fig, update, interval=1, frames=10000, repeat=False, blit=True)
+read_data_thread = threading.Thread(target=read_data, daemon=True)
+read_data_thread.start()
+
+ani = animation.FuncAnimation(fig, update, interval=1, repeat=False, blit=True)
 plt.show()
 ser.close()
+read_data_thread.join()
+
+
