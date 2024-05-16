0375284f27a811f049a987f5cae20400f574b722
smile <kingsanhu@gmail.com>
2024-05-16 15:01:34 +0800

    single loop

--- main.py
+++ main.py
@@ -23,28 +23,36 @@ def read_data():
     global iterate_counter
     while True:
         angle_data.append(float(ANGLE_VALUES[iterate_counter % len(ANGLE_VALUES)]))
-        dist_data.append(get_dist())
+        # temp_data = np.array([get_dist() for _ in range(2)])
+        # for _ in range(4):
+        #     get_dist()
+        dist_data.append(get_dist() - 0.4)
         iterate_counter += 1
 
 
 TDC_INNER_REF_CLK = 5  # in MHz
-MIRROR_ANGLE_LIMIT = 40.0 / 180.0 * np.pi  # in radians
-MIRROR_FREQ = 80.0  # in Hz
+MIRROR_ANGLE_LIMIT_DEG = 40.0
+MIRROR_ANGLE_LIMIT_ARC = MIRROR_ANGLE_LIMIT_DEG / 180.0 * np.pi  # in radians
+MIRROR_FREQ = 78.8  # in Hz
 MEASURE_FREQ = 16666.6  # in Hz
 iterate_counter = 0
 
-SAMPLE_VALUES = np.linspace(-np.sin(MIRROR_ANGLE_LIMIT), np.sin(MIRROR_ANGLE_LIMIT),  int(MEASURE_FREQ / MIRROR_FREQ))
-ANGLE_VALUES = np.arcsin(SAMPLE_VALUES)
+SAMPLE_VALUES = np.linspace(-np.sin(MIRROR_ANGLE_LIMIT_ARC), np.sin(MIRROR_ANGLE_LIMIT_ARC), int(MEASURE_FREQ / MIRROR_FREQ))
+ANGLE_VALUES = np.repeat(3 * np.arcsin(SAMPLE_VALUES), 1)
 
 ser = serial.Serial('COM11', 921600, timeout=1)
 fig, ax = plt.subplots(layout='constrained', subplot_kw=dict(projection='polar'))
 
-angle_data = deque([0.0] * 300, maxlen=300)
-dist_data = deque([0.0] * 300, maxlen=300)
+angle_data = deque([0.0] * 600, maxlen=600)
+dist_data = deque([0.0] * 600, maxlen=600)
 scat = ax.scatter(angle_data, dist_data, c='r', marker='^', label='scanned points', animated=True)
 
-ax.set_xticks(np.linspace(0, 2 * np.pi, 12, endpoint=False))
-ax.set_yticks(np.arange(0.0, 1.5, 0.1))
+ax.set_xticks(np.linspace(0, 2 * np.pi, 24, endpoint=False))
+ax.set_yticks(np.arange(0.0, 1.0, 0.1))
+# ax.set_thetamin(-2 * (MIRROR_ANGLE_LIMIT_DEG))
+# ax.set_thetamax(2 * (MIRROR_ANGLE_LIMIT_DEG))
+ax.set_theta_direction(-1)
+ax.set_theta_offset(np.pi / 2)
 ax.set_title("Scan result of RangeFinder")
 fig.legend(loc='outside lower center')
 
