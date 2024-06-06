63ad35be289bd601327d0e1b4e3fb4162a04b49e
Smile <smile@Magic.local>
2024-06-06 19:01:30 +0800

    Modify code for checking the received data

--- SerialCommunicationTest.py
+++ SerialCommunicationTest.py
@@ -1,7 +1,9 @@
 import serial
 
-ser = serial.Serial('COM4', 115200, timeout=1)
-ser.write(b'Hello World\n')
-receive_data = ser.readline()
-print("Receive data: ", receive_data)
+ser = serial.Serial('/dev/tty.usbserial-13130', 3000000, timeout=1)
+# ser.write(b'Hello World\n')
+for _ in range(100):
+    receive_data = ser.read(8)
+    print("Receive data: ", receive_data)
+
 ser.close()
