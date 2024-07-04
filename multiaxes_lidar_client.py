cf6b9b7dae16d84d0171d7174d13932f17fba063
Smile <smile@Magic.local>
2024-07-04 13:27:47 +0800

    Add multi-threading back
    Use variable data to capture a snapshot of the (x, y, z, colors) to
    avoid data racing in parallel-computing

--- multiaxes_lidar_client.py
+++ multiaxes_lidar_client.py
@@ -15,28 +15,27 @@ def read_data():
     """
     thread function for pushing one frame data into the deque continuously
     """
-    global i
+    global data
     while True:
         # fast_axis_angle, slow_axis_angle, dist = get_data(ser)
-        fast_axis_angle = np.pi / 4.0
-        slow_axis_angle = np.pi / 4.0
-        dist = (i + 1) / 1000.0
-        i = (i + 1) % 2000
+        fast_axis_angle = np.random.uniform(0.0, 2 * np.pi)
+        slow_axis_angle = np.random.uniform(0.0, np.pi / 2.0)
+        dist = np.random.uniform(0.0, MAX_DIST)
 
         # if the distance is larger than MAX_DIST, abandon this data
         if dist > MAX_DIST:
             continue
 
-        x_data = 1.0
-        y_data = 1.0
-        z_data = i / 10.0
-        i = (i + 1) % 10
-        # print(x_data, y_data, z_data)
-        colors.append(cmap(norm(z_data)))
+        x_data = dist * np.cos(slow_axis_angle) * np.sin(fast_axis_angle)
+        y_data = dist * np.cos(slow_axis_angle) * np.cos(fast_axis_angle)
+        z_data = dist * np.sin(slow_axis_angle)
+
+        colors.append(dist)
         x.append(x_data)
         y.append(y_data)
         z.append(z_data)
-        # print(dist)
+
+        data = np.vstack([np.array(x), np.array(y), np.array(z), np.array(colors)])
 
 
 def update_figure(frame):
@@ -45,38 +44,27 @@ def update_figure(frame):
     :param frame: one frame for the figure
     :return: updated artists
     """
-    fast_axis_angle = np.random.uniform(0.0, 2*np.pi)
-    slow_axis_angle = np.random.uniform(0.0, np.pi/2.0)
-    dist = np.random.uniform(0.0, MAX_DIST)
-
-    x_data = dist * np.cos(slow_axis_angle) * np.sin(fast_axis_angle)
-    y_data = dist * np.cos(slow_axis_angle) * np.cos(fast_axis_angle)
-    z_data = dist * np.sin(slow_axis_angle)
-    colors.append(cmap(norm(z_data)))
-    x.append(x_data)
-    y.append(y_data)
-    z.append(z_data)
-
-    scat.set_color(colors)  # update the color of each scattered point
-    scat._offsets3d = (x, y, z)
+    scat.set_color(cmap(norm(data[3])))  # update the color of each scattered point
+    scat._offsets3d = data[:3]
     bm.update()
     return [scat, ]
 
 
 MAX_DIST = 2.0
-i = 1
+NUM_POINTS_ON_VIEW = 100
 
 # ser = serial.Serial('/dev/tty.usbserial-1310', 3000000, timeout=1)
 fig, ax = plt.subplots(layout='constrained', subplot_kw=dict(
     projection='3d'))
 
-x = deque([0.0] * 100, maxlen=100)
-y = deque([0.0] * 100, maxlen=100)
-z = deque([0.0] * 100, maxlen=100)
+x = deque([0.0] * NUM_POINTS_ON_VIEW, maxlen=NUM_POINTS_ON_VIEW)
+y = deque([0.0] * NUM_POINTS_ON_VIEW, maxlen=NUM_POINTS_ON_VIEW)
+z = deque([0.0] * NUM_POINTS_ON_VIEW, maxlen=NUM_POINTS_ON_VIEW)
+data = None
+colors = deque([0.0] * NUM_POINTS_ON_VIEW, maxlen=NUM_POINTS_ON_VIEW)
 cmap = plt.colormaps['viridis']
 norm = mcolors.Normalize(vmin=0, vmax=MAX_DIST)
-colors = deque([cmap(0.0)] * 100, maxlen=100)
-scat = ax.scatter(x, y, z, c=colors, marker='o', edgecolor='none',
+scat = ax.scatter(x, y, z, c='r', marker='o', edgecolor='none',
                   label='scanned points')
 ax.set_xlim(2.0, 0.0)
 ax.set_ylim(2.0, 0.0)
@@ -88,11 +76,11 @@ ax.view_init(elev=20, azim=45)
 
 bm = BlitManager(fig.canvas, [scat, ])
 
-# read_data_thread = threading.Thread(target=read_data, daemon=True)
-# read_data_thread.start()
+read_data_thread = threading.Thread(target=read_data, daemon=True)
+read_data_thread.start()
 
 ani = animation.FuncAnimation(fig, update_figure, interval=1,
                               repeat=False)
 plt.show()
-# read_data_thread.join()
+read_data_thread.join()
 # ser.close()
